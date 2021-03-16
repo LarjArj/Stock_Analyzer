671df5613e654d86609e746def2d95fcf3287365
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-03-16 16:11:46 -0400

    JS-> html integration

--- stock_analyzer.py
+++ stock_analyzer.py
@@ -2,11 +2,30 @@ from alpha_vantage.timeseries import TimeSeries
 import requests
 import json
 import math
+import time
+import random
 
-def writeToJson():
-    stocks = getStockInfo()
-    with open('stocks.json', 'w') as fp:
+
+def writeToJson(stock):
+    stocks = getStockInfo(stock)
+    with open('stocksFile.json', 'w') as fp:
         json.dump(stocks, fp)
+
+
+def getRandomStocks(stocks,inputtedStock):
+    seen = {}
+    length = len(stocks)
+    output = []
+    for i in range(10):
+        
+        idx = random.randint(0,length)
+        if idx in seen and stocks[idx] == inputtedStock:
+            continue
+        seen[idx] = True
+        output.append(stocks[idx])
+    return output
+
+
 
 
 
@@ -15,17 +34,22 @@ def getStocks():
     return f.read().splitlines()
 
 
-def getStockInfo():
+def getStockInfo(inputtedStock):
 
     stocks = getStocks()
+    randomStocks = getRandomStocks(stocks,inputtedStock)
 
     stockInfo = {}
-    for stock in stocks:
+    for stock in randomStocks:
+        print(stock)
+        time.sleep(60)
+        if getAnnualizedReturn(stock) == None:
+            continue
         avgPrice,annualizedReturn,standardDeviation = getAnnualizedReturn(stock)
 
         stockInfo[stock] = {"avgPrice":avgPrice,"annualizedReturn": annualizedReturn,"standardDeviation":standardDeviation}
-        print(stock) 
-        print(stockInfo[stock])
+        #print(stock) 
+       #print(stockInfo[stock])
 
 
     return stockInfo
@@ -65,33 +89,35 @@ def getAnnualizedReturn(stock):
         month2Price[key] = stockTable[key]
         counter += 1
         
-    return month2Price
-    ##prices = []
+    if counter > 1:
+        return calculateStatistics(month2Price)
+    else:
+        return None
+    ##prices = 
 
 
+def calculateStatistics(month2Price):
+    prices = []
+    for key in month2Price:
+        prices.append(int(float(month2Price[key])))
 
-    # for key in month2Price:
-    #     prices.append(int(float(month2Price[key])))
+    annualizedReturn = (prices[0] - prices[-1]) / prices[0]
+    isNegative = annualizedReturn < 0 
+    if isNegative:
+        annualizedReturn *= -1
+    annualizedReturn *= 100
 
-    # annualizedReturn  = (prices[0] - prices[-1]) / prices[0]
-    # isNegative = annualizedReturn < 0
-    # if isNegative:
-    #     annualizedReturn *= -1 
+    averagePrice = 0
+    for price in prices:
+        averagePrice += price
+    averagePrice /= len(prices)
 
-    # annualizedReturn *=  100
+    standardDeviation = 0
+    for price in prices:
+        standardDeviation += (price - averagePrice) ** 2
+    standardDeviation /= len(prices)
+    return (averagePrice,annualizedReturn,math.sqrt(standardDeviation))
 
-    # averagePrice = 0 
-    # for price in prices:
-    #     averagePrice += price
-    # averagePrice /= len(prices)
-
-    # standardDeviation = 0
-    # for price in prices:
-    #     standardDeviation += (price - averagePrice) ** 2
-
-    # standardDeviation /= len(prices)
-
-    # return (averagePrice,annualizedReturn,math.sqrt(standardDeviation))
 
 
 
